257469f231e07336669d890d77e81faacbbb4484
morenosaputra05 <ssaputramoreno@gmail.com>
2023-12-10 03:41:11 -0500

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    borderWidth:{
+      "3":"3px",
+    },
     extend: {
       margin:{
         "mobile" : "70px",
         "10px": "10px",
+        "50px": "50px",
         "100px": "100px",
       },
       padding: {
@@ -22,6 +26,7 @@ module.exports = {
       },
       colors: {
         "smoke": "#DBDBDB",
+        "smoke-black": "#0D0D0D",
       },
       fontFamily: {
         "inter":['Inter', 'sans-serif'],
@@ -29,17 +34,26 @@ module.exports = {
       height: {
         "desk": "375px",
         "mobile": "250px",
+        "100px": "100px",
         "120px": "120px",
         "256px": "256px",
+        "590px":"550px",
         "410px": "410px",
         "390px": "390px",
         "577px": "577px",
+        "78%":"78%",
+        "80%":"80%",
+        "95%": "95%",
       },
       width: {
+        "2px": "2px",
         "190px": "190px",
+        "380px": "380px",
         "410px": "410px",
         "472px": "472px",
+        "1120px": "1120px",
         "1314px": "1314px",
+        "95%": "95%",
       },
       letterSpacing:{
         "5%": "1.2px",
@@ -47,6 +61,8 @@ module.exports = {
       },
       backgroundSize:{
         "90%": "90%",
+        "95%": "95%",
+        "100%":"100%",
       }
     },
   },
